752b565b267d091d60c5263f26b40b71301d7083
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2021-10-25 22:35:12 +0200

    :white_check_mark: Add unit tests for actions

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,255 @@
+import wave from '../'
+
+const flushPromises = () => new Promise((resolve) => setTimeout(resolve))
+
+describe('success', () => {
+    const makeModule = () =>
+        wave<string, string>(
+            {
+                getDataKey: (data) => data.slice(0, 1),
+                getParamsKey: (data) => data.slice(-1),
+            },
+            {
+                add: (data) => Promise.resolve(`added ${data}`),
+                edit: (oldData, newData) =>
+                    Promise.resolve(`edited ${newData}`),
+                fetch: (params) => Promise.resolve([`fetched ${params}`]),
+                refetch: (params) => Promise.resolve([`refetched ${params}`]),
+                remove: () => Promise.resolve(),
+            }
+        )
+
+    test('add', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: 'adding' } },
+            status: {},
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { a: { data: 'added wave', status: null } },
+            status: {},
+        })
+    })
+
+    test('edit', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.edit('added wave', 'tsunami')
+
+        expect(module.state).toEqual({
+            items: { t: { data: 'tsunami', status: 'editing' } },
+            status: {},
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { e: { data: 'edited tsunami', status: null } },
+            status: {},
+        })
+    })
+
+    test('fetch', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.fetch('tsunami')
+
+        expect(module.state).toEqual({
+            items: { a: { data: 'added wave', status: null } },
+            status: { i: 'fetching' },
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: {
+                a: { data: 'added wave', status: null },
+                f: { data: 'fetched tsunami', status: null },
+            },
+            status: { i: 'fetched' },
+        })
+    })
+
+    test('refetch', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.refetch('tsunami')
+
+        expect(module.state).toEqual({
+            items: { a: { data: 'added wave', status: null } },
+            status: { i: 'refetching' },
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: {
+                r: { data: 'refetched tsunami', status: null },
+            },
+            status: { i: 'refetched' },
+        })
+    })
+
+    test('remove', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.remove('added wave')
+
+        expect(module.state).toEqual({
+            items: { a: { data: 'added wave', status: 'removing' } },
+            status: {},
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: {},
+            status: {},
+        })
+    })
+})
+
+describe('error', () => {
+    const makeModule = () =>
+        wave<string, string>(
+            {
+                getDataKey: (data) => data.slice(0, 1),
+                getParamsKey: (data) => data.slice(-1),
+            },
+            {
+                add: (data) => Promise.reject(`added ${data}`),
+                edit: (oldData, newData) => Promise.reject(`edited ${newData}`),
+                fetch: (params) => Promise.reject([`fetched ${params}`]),
+                refetch: (params) => Promise.reject([`refetched ${params}`]),
+                remove: (data) => Promise.reject(`removed ${data}`),
+            }
+        )
+
+    test('add', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: 'adding' } },
+            status: {},
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: Error('added wave') } },
+            status: {},
+        })
+    })
+
+    test('edit', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.edit('wave', 'tsunami')
+
+        expect(module.state).toEqual({
+            items: { t: { data: 'tsunami', status: 'editing' } },
+            status: {},
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { t: { data: 'tsunami', status: Error('edited tsunami') } },
+            status: {},
+        })
+    })
+
+    test('fetch', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.fetch('tsunami')
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: Error('added wave') } },
+            status: { i: 'fetching' },
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: Error('added wave') } },
+            status: { i: Error('fetched tsunami') },
+        })
+    })
+
+    test('refetch', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.refetch('tsunami')
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: Error('added wave') } },
+            status: { i: 'refetching' },
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: Error('added wave') } },
+            status: { i: Error('refetched tsunami') },
+        })
+    })
+
+    test('remove', async () => {
+        const module = makeModule()
+
+        module.add('wave')
+
+        await flushPromises()
+
+        module.remove('wave')
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: 'removing' } },
+            status: {},
+        })
+
+        await flushPromises()
+
+        expect(module.state).toEqual({
+            items: { w: { data: 'wave', status: Error('removed wave') } },
+            status: {},
+        })
+    })
+})
