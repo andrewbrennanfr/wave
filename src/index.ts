f74ea77182a344f52a45f66d821098bf72274453
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2021-10-30 01:23:30 +0200

    :label: Export public types directly

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import { PublicItems } from './item/item-types'
 import { useModule } from './module/module-tools'
 import { Module as _Module } from './module/module-types'
+import { PublicStatuses } from './status/status-types'
 
+export type Items<D> = PublicItems<D>
 export type Module<D, P> = _Module<D, P>
-
-export type Items<D, P> = Module<D, P>['state']['items']
-export type Statuses<D, P> = Module<D, P>['state']['statuses']
+export type Statuses<D> = PublicStatuses<D>
 
 export default useModule
