2f4a910c46d25288dd8ec56d7ed67ed1077df9df
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2021-10-31 09:35:46 +0100

    :label: Remove public module type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { makeModule } from './module/module-tools'
-import { Module as _Module } from './module/module-types'
 
 //==============================================================================
-
-export type Module<D, P> = _Module<D, P>
 
 export default makeModule
