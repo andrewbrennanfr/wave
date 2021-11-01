88bf246cbb00ad015aa61e531d0c71423605392c
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2021-11-01 14:37:34 +0100

    :label: Expose more public types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { makeModule } from './module/module-tools'
+import { Module as _Module } from './module/module-types'
 
 //==============================================================================
+
+export type Module<D, P> = _Module<D, P>
+
+export type State<D, P> = Module<D, P>['state']
+
+export type Items<D, P> = State<D, P>['items']
+
+export type Item<D, P> = NonNullable<Items<D, P>[string]>
+
+export type Statuses<D, P> = State<D, P>['statuses']
+
+export type Status<D, P> = NonNullable<Statuses<D, P>[string]>
 
 export default makeModule
